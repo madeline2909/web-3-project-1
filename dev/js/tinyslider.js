ad3abc91f2f59857012bf3f078fa33c7efcff4dc
madeline2909 <63311619+madeline2909@users.noreply.github.com>
2020-08-16 18:52:41 -0400

    tiny slider customized

--- dev/js/tinyslider.js
+++ dev/js/tinyslider.js
@@ -4,14 +4,15 @@ $(document).ready(function () {
   console.log($(".slides").hasClass("my-slider"));
   if ($(".slides").hasClass("my-slider")) {
     tns({
+        navPosition: "bottom",
         container: '.my-slider',
         items: 1,
+        mouseDrag: true,
         slideBy: "page",
-        navPosition: "bottom",
         "edgePadding": 50,
         "gutter": 20,
         controls: true,
-        autoplay: true,
+        controlsPosition:"bottom",
         responsive: {
             768: {
               "gutter": 20,
@@ -30,4 +31,5 @@ $(document).ready(function () {
     console.log("No TinySlider found!");
   }
 });
+
 
