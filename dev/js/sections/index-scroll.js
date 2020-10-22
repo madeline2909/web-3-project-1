85d9843933eebd239715ed39cf35e016303fe931
madeline2909 <63311619+madeline2909@users.noreply.github.com>
2020-10-22 22:17:26 +0700

    index animation timing

--- dev/js/sections/index-scroll.js
+++ dev/js/sections/index-scroll.js
@@ -1,10 +1,14 @@
-import {gsap} from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {
+    gsap
+} from "gsap";
+import {
+    ScrollTrigger
+} from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 //HOME section-titles slide from top
-export function indexTitleAnimation(){
+export function indexTitleAnimation() {
     ScrollTrigger.matchMedia({
 
         // desktop
@@ -29,55 +33,58 @@ export function indexTitleAnimation(){
             ScrollTrigger.create({
                 trigger: "#home-intro-img",
                 id: "intro",
+                start: "top 50%",
                 //markers: true,
-                scrub: 1,
-                toggleActions: "restart none none none",
+                //scrub: 1,
+                toggleActions: "restart none none reverse",
                 animation: introTimeline
             })
 
             var cultureTimeline = gsap.timeline();
             cultureTimeline
-            .from("#img4", {
-                alpha: 0
-            }, "culture anim 1")
-            .from("#culture", {
-                xPercent: 100,
-                alpha: 0
-            }, "culture anim 1")
-            .from("#home-culture-anim", {
-                duration: 0.5,
-                yPercent: 10,
-                alpha: 0
-            })
+                .from("#img4", {
+                    alpha: 0
+                }, "culture anim 1")
+                .from("#culture", {
+                    xPercent: 100,
+                    alpha: 0
+                }, "culture anim 1")
+                .from("#home-culture-anim", {
+                    duration: 0.5,
+                    yPercent: 10,
+                    alpha: 0
+                })
             ScrollTrigger.create({
                 trigger: "#home-culture",
                 id: "culture",
                 //markers: true,
-                start: "top 70%",
+                start: "top 50%",
                 end: "85% bottom",
-                scrub: 1,
-                toggleActions: "restart none none none",
+                //scrub: 1,
+                toggleActions: "restart none none reverse",
+
                 animation: cultureTimeline
             })
 
             var artTimeline = gsap.timeline();
             artTimeline.from("#art", {
-                xPercent: -100,
-                alpha: 0
-            })
-            .from("#home-art-anim", {
-                duration: 0.5,
-                yPercent: 10,
-                alpha: 0
-            })
+                    xPercent: -100,
+                    alpha: 0
+                })
+                .from("#home-art-anim", {
+                    duration: 0.5,
+                    yPercent: 10,
+                    alpha: 0
+                })
             ScrollTrigger.create({
                 trigger: "#home-art",
                 id: "art",
                 // markers: true,
                 start: "top 70%",
                 end: "85% bottom",
-                scrub: 1,
-                toggleActions: "restart none none none",
+                //scrub: 1,
+                toggleActions: "restart none none reverse",
+
                 animation: artTimeline
             })
         },
@@ -106,123 +113,60 @@ export function indexTitleAnimation(){
 
             ScrollTrigger.create({
                 trigger: "#home-intro-img",
-                scrub: 1,
-                toggleActions: "restart none none none",
+                //scrub: 1,
+                toggleActions: "restart none none reverse",
+
                 animation: introTimeline
             })
-            
+
             var cultureTimeline = gsap.timeline();
             cultureTimeline
-            .from("#img4", {
-                alpha: 0
-            })
-            .from("#culture", {
-                xPercent: 100,
-                alpha: 0
-            }, "-=.5")
-            .from("#home-culture-anim", {
-                duration: 0.5,
-                yPercent: 10,
-                alpha: 0
-            })
+                .from("#img4", {
+                    alpha: 0
+                })
+                .from("#culture", {
+                    xPercent: 100,
+                    alpha: 0
+                }, "-=.5")
+                .from("#home-culture-anim", {
+                    duration: 0.5,
+                    yPercent: 10,
+                    alpha: 0
+                })
             ScrollTrigger.create({
                 trigger: "#home-culture",
                 id: "culture",
                 // markers: true,
                 // start: "-200 top",
                 end: "bottom 80%",
-                scrub: 1,
-                toggleActions: "restart none none none",
+                //scrub: 1,
+                toggleActions: "restart none none reverse",
+
                 animation: cultureTimeline
             })
 
             var artTimeline = gsap.timeline();
             artTimeline.from("#home-art-anim", {
-                duration: 0.5,
-                yPercent: 10,
-                alpha: 0
-            })
-            .from("#art", {
-                xPercent: -100,
-                alpha: 0
-            })
-            
+                    duration: 0.5,
+                    yPercent: 10,
+                    alpha: 0
+                })
+                .from("#art", {
+                    xPercent: -100,
+                    alpha: 0
+                })
+
             ScrollTrigger.create({
                 trigger: "#home-art",
                 id: "art",
                 // markers: true,
                 start: "-200 top",
                 end: "top bottom",
-                scrub: 1,
-                toggleActions: "restart none none none",
+                //scrub: 1,
+                toggleActions: "restart none none reverse",
+
                 animation: artTimeline
             })
         }
     });
 }
-
-//paragraphs animation HOME
-// export function indexTextAnimation(){
-//     gsap.from(".index-caption", {
-//         duration: 0.5,
-//         yPercent: 10,
-//         alpha: 0,
-//         scrollTrigger: {
-//             //scrub:true,
-//             toggleActions: "restart reverse restart reverse",
-//             trigger: "#home-intro-p",
-//             start: "top 70%",
-//             //markers: true,
-//             id: "index caption"
-//         }
-//     })
-
-//     gsap.from("#home-culture-anim", {
-//         duration: 0.5,
-//         yPercent: 10,
-//         alpha: 0,
-//         scrollTrigger: {
-//             //scrub:true,
-//             toggleActions: "restart reverse restart reverse",
-//             trigger: "#home-culture-para",
-//             start: "top 70%",
-//             //markers: true,
-//             id: "culture"
-//         }
-//     })
-
-//     gsap.from("#home-art-anim", {
-//         duration: 0.5,
-//         yPercent: 10,
-//         alpha: 0,
-//         scrollTrigger: {
-//             //scrub:true,
-//             toggleActions: "restart reverse restart reverse",
-//             trigger: "#home-art",
-//             start: "top 70%",
-//             //markers: true,
-//             id: "culture"
-//         }
-//     })
-// }
-
-// const indexTextAnimTL = gsap.timeline();
-// indexTextAnimTL
-// .from(".index-caption", {
-//     duration: 0.5,
-//     yPercent: 10,
-//     alpha: 0
-// });
-
-// export function indexTextAnimation(){
-
-//     ScrollTrigger.create({
-//         //markers: true,
-//         animation: indexTextAnimTL,
-//         toggleActions: "restart reverse restart reverse",
-//         trigger: ".scroll-animation",
-//         start: "top 70%",
-//         scrub:1,
-//         id: "index caption"
-//     });
-// }
